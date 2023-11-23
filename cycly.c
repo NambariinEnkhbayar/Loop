6ba0e04e74771f2e00be73fc6ecced82ca8785f7
NambariinEnkhbayar <151820629+NambariinEnkhbayar@users.noreply.github.com>
2023-11-23 16:14:29 +0800

    Add files via upload

---
+++ cycly.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "cs50.h"
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    } while (n<1);
+    for (int j = 0; j < n; j++)
+    {
+        printf("#");
+    }
+}
